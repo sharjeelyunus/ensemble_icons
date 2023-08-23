3956883a3645967275030f49c87b98df0d33f2a0
Vu Le <vu@ensembleui.com>
2023-08-22 21:54:18 -0700

    initial check in for icons

---
+++ lib/remixicon.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+
+/// this class should be generated
+class Remix {
+
+  static final Map<String, IconData> iconMap = {
+    'time_line': Remix.time_line,
+    'first_aid_kit_line': Remix.first_aid_kit_line,
+    'image_2_line': Remix.image_2_line,
+    'thermometer_line': Remix.thermometer_line,
+    'shield_check_line': Remix.shield_check_line,
+    'road_map_line': Remix.road_map_line,
+    'add_circle_line': Remix.add_circle_line,
+    'user_line': Remix.user_line,
+    'questionnaire_line': Remix.questionnaire_line,
+    'stack_line': Remix.stack_line,
+    'focus_3_line': Remix.focus_3_line,
+
+    'home_4_line': Remix.home_4_line,
+    'user_star_line': Remix.user_star_line,
+    'list_check': Remix.list_check,
+    'account_circle_line': Remix.account_circle_line,
+  };
+
+  static const _ff = 'Remix';
+  static const _fp = 'ensemble_icons';
+
+  static const IconData image_2_line =
+      IconData(0xEE45, fontFamily: _ff, fontPackage: _fp);
+  static const IconData time_line =
+      IconData(0xF20F, fontFamily: _ff, fontPackage: _fp);
+  static const IconData thermometer_line =
+      IconData(0xF203, fontFamily: _ff, fontPackage: _fp);
+  static const IconData first_aid_kit_line =
+      IconData(0xED37, fontFamily: _ff, fontPackage: _fp);
+  static const IconData shield_check_line =
+      IconData(0xF100, fontFamily: _ff, fontPackage: _fp);
+  static const IconData road_map_line =
+      IconData(0xF08E, fontFamily: _ff, fontPackage: _fp);
+  static const IconData add_circle_line =
+      IconData(0xEA11, fontFamily: _ff, fontPackage: _fp);
+  static const IconData user_line =
+      IconData(0xF264, fontFamily: _ff, fontPackage: _fp);
+  static const IconData questionnaire_line =
+      IconData(0xF048, fontFamily: _ff, fontPackage: _fp);
+  static const IconData stack_line =
+      IconData(0xF181, fontFamily: _ff, fontPackage: _fp);
+  static const IconData focus_3_line =
+      IconData(0xED4C, fontFamily: _ff, fontPackage: _fp);
+
+  static const IconData home_4_line =
+      IconData(0xEE1D, fontFamily: _ff, fontPackage: _fp);
+  static const IconData user_star_line =
+      IconData(0xF276, fontFamily: _ff, fontPackage: _fp);
+  static const IconData list_check =
+      IconData(0xEEBA, fontFamily: _ff, fontPackage: _fp);
+  static const IconData account_circle_line =
+      IconData(0xEA09, fontFamily: _ff, fontPackage: _fp);
+}
