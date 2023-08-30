b300798bd2b27f825566381481d24a6cb6fed01b
Amin <amin-nas@users.noreply.github.com>
2023-08-30 15:13:04 -0400

    Update remixicon.dart

--- lib/remixicon.dart
+++ lib/remixicon.dart
@@ -4,34 +4,39 @@ import 'package:flutter/cupertino.dart';
 class Remix {
 
   static final Map<String, IconData> iconMap = {
-    'time_line': Remix.time_line,
-    'first_aid_kit_line': Remix.first_aid_kit_line,
-    'image_2_line': Remix.image_2_line,
-    'thermometer_line': Remix.thermometer_line,
-    'shield_check_line': Remix.shield_check_line,
-    'road_map_line': Remix.road_map_line,
-    'add_circle_line': Remix.add_circle_line,
-    'user_line': Remix.user_line,
-    'questionnaire_line': Remix.questionnaire_line,
-    'stack_line': Remix.stack_line,
-    'focus_3_line': Remix.focus_3_line,
-
-    'home_4_line': Remix.home_4_line,
-    'user_star_line': Remix.user_star_line,
-    'list_check': Remix.list_check,
     'account_circle_line': Remix.account_circle_line,
-
-    'arrow_up_s_line': Remix.arrow_up_s_line,
-    'arrow_right_s_line': Remix.arrow_right_s_line,
+    'add_circle_line': Remix.add_circle_line,
     'arrow_down_s_line': Remix.arrow_down_s_line,
     'arrow_left_s_line': Remix.arrow_left_s_line,
+    'arrow_right_s_line': Remix.arrow_right_s_line,
+    'arrow_up_s_line': Remix.arrow_up_s_line,
+    'bar_chart_2_line': Remix.bar_chart_2_line,
+    'customer_service_2_line': Remix.customer_service_2_line,
+    'first_aid_kit_line': Remix.first_aid_kit_line,
+    'flashlight_fill': Remix.flashlight_fill,
+    'focus_3_line': Remix.focus_3_line,
+    'home_line': Remix.home_line,
+    'home_4_line': Remix.home_4_line,
+    'image_2_line': Remix.image_2_line,
+    'list_check': Remix.list_check,
     'more_line': Remix.more_line,
-    'more_2_line': Remix.more_2_line
+    'more_2_line': Remix.more_2_line,
+    'movie_line': Remix.movie_line,
+    'questionnaire_line': Remix.questionnaire_line,
+    'shield_check_line': Remix.shield_check_line,
+    'stack_line': Remix.stack_line,
+    'road_map_line': Remix.road_map_line,
+    'thermometer_line': Remix.thermometer_line,
+    'time_line': Remix.time_line,
+    'user_line': Remix.user_line,
+    'user_star_line': Remix.user_star_line
   };
 
   static const _ff = 'Remix';
   static const _fp = 'ensemble_icons';
 
+  static const IconData customer_service_2_line = 
+      IconData(0xEC0C, fontFamily: _ff, fontPackage: _fp);
   static const IconData image_2_line =
       IconData(0xEE45, fontFamily: _ff, fontPackage: _fp);
   static const IconData time_line =
@@ -54,7 +59,8 @@ class Remix {
       IconData(0xF181, fontFamily: _ff, fontPackage: _fp);
   static const IconData focus_3_line =
       IconData(0xED4C, fontFamily: _ff, fontPackage: _fp);
-
+  static const IconData home_line =
+      IconData(0xEE2B, fontFamily: _ff, fontPackage: _fp);
   static const IconData home_4_line =
       IconData(0xEE1D, fontFamily: _ff, fontPackage: _fp);
   static const IconData user_star_line =
@@ -63,7 +69,6 @@ class Remix {
       IconData(0xEEBA, fontFamily: _ff, fontPackage: _fp);
   static const IconData account_circle_line =
       IconData(0xEA09, fontFamily: _ff, fontPackage: _fp);
-
   static const IconData arrow_up_s_line =
       IconData(0xEA78, fontFamily: _ff, fontPackage: _fp);
   static const IconData arrow_right_s_line =
@@ -76,5 +81,10 @@ class Remix {
       IconData(0xEF79, fontFamily: _ff, fontPackage: _fp);
   static const IconData more_2_line =
       IconData(0xEF77, fontFamily: _ff, fontPackage: _fp);
-
+  static const IconData movie_line =
+      IconData(0xEF81, fontFamily: _ff, fontPackage: _fp);
+  static const IconData bar_chart_2_line =
+      IconData(0xEA96, fontFamily: _ff, fontPackage: _fp);
+  static const IconData flashlight_fill =
+      IconData(0xED3C, fontFamily: _ff, fontPackage: _fp);
 }
